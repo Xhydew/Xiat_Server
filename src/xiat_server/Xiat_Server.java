9ecda4f8f3c3746db387dd2f034b0809d15749f2
Xhydew <sidewinder476@gmail.com>
2015-11-17 18:11:30 +0100

    Recibe y devuelve por sys.out

--- src/xiat_server/Xiat_Server.java
+++ src/xiat_server/Xiat_Server.java
@@ -5,17 +5,43 @@
  */
 package xiat_server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
- * @author Joan Galiana Magán
+ * @author Joan Galiana MagÃ¡n
  */
 public class Xiat_Server {
 
+    private final static int puerto = 5050;
+
+    private String teclao(Scanner telcao) {
+        return telcao.nextLine();
+    }
+
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        ServerSocket servSoc = new ServerSocket(puerto); //Crea Socket de servidor
+
+        Socket cliSock = servSoc.accept();//Establece conexion con el cliente
+        System.err.println("Conectado");//Muestra por el canal de error al conectar con cliente
+
+        PrintWriter fuera = new PrintWriter(cliSock.getOutputStream());//Canal para enviar
+        Scanner dentro = new Scanner(cliSock.getInputStream());//Canal para recibir
+
+        do {
+            System.out.println("Recibido: " + dentro.nextLine()
+                    + "\t \t de :" + cliSock.getInetAddress());// Muesta recibido, con direccion del 
+        } while (cliSock.isConnected());//mientras que exista conexion
+
     }
-    
+
 }
