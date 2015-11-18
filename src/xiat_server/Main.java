6f6c24a38fe0e2e64024688a7204598582bb6c7b
Joan <sidewinder476@gmail.com>
2015-11-18 13:23:14 +0100

    Servidor es un objeto Funciona conecta 2 usuarios Fuentes añadidas

---
+++ src/xiat_server/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package xiat_server;
+
+/**
+ *
+ * @author Joan Galiana Magán
+ */
+public class Main {
+    public static void main(String[] args) {
+        Servidor serv1=new Servidor();
+    }
+}
